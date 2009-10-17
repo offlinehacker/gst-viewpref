841dade4300c1d3c99c3be4f6b34adc2c45128b6
fthiery <fthiery@ab798842-a508-11de-b195-ab392a2a73f5>
2009-10-17 15:21:31 +0000

    Adding gst-viewperf to PATH

--- install_clutter_1.0.sh
+++ install_clutter_1.0.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 sudo ln -s `pwd`/src-clutter-1.0 /usr/lib/python2.5/site-packages/gstviewperf
+sudo ln -s `pwd`/src-clutter-1.0/gst-viewperf /usr/bin/gst-viewperf
+sudo chmod +x /usr/bin/gst-viewperf
--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sudo rm /usr/lib/python2.5/site-packages/gstviewperf
+sudo rm /usr/bin/gst-viewperf
